014d3d8a41d25f532d1be10d706ae159453a1f14
zerochae <zerochae@kakao.com>
2022-03-23 21:31:51 +0900

    Feat: customhook useStage

---
+++ src/Utils/useStage.ts
@@ -0,0 +1,37 @@
+import * as C from "Constants/index";
+import * as T from "Types/index";
+
+import { useEffect, useState } from "react";
+
+const useStage = (player: T.Player, resetPlayer: () => void) => {
+  const [stage, setStage] = useState(C.createStage());
+
+  type StageCell = [string | number, string];
+  type Stage = StageCell[][];
+
+  useEffect(() => {
+    if (!player.pos) return;
+
+    const updateStage = (prevStage: Stage): Stage => {
+      const newStage = prevStage.map(
+        (row) =>
+          row.map((cell) =>
+            cell[1] === "clear" ? [0, "clear"] : cell
+          ) as StageCell[]
+      );
+      player.tetromino.forEach((row, y) => {
+        row.forEach((value, x) => {
+          if (value !== 0) {
+            newStage[y + player.pos.y][x + player.pos.x] = [
+              value,
+              `${player.collided ? "merged" : "clear"}`,
+            ];
+          }
+        });
+      });
+      return newStage;
+    };
+
+    setStage((prev) => updateStage(prev));
+  }, [player.collided, player.pos?.x, player.pos?.y, player.tetromino]);
+};

baaf51f34cdf3e0ceb4bdd3e87f828f5f0002898
zerochae <zerochae@kakao.com>
2022-03-23 19:22:47 +0900

    Feat: constants/setup

---
+++ src/Constants/Setup/index.ts
@@ -0,0 +1,32 @@
+export const STAGE_WIDTH = 12;
+export const STAGE_HEIGHT = 20;
+export const ROWPOINTS = [30, 100, 300, 1200];
+
+export const TETROMINOS = {
+  0: { shape: [[0]], color: "0,0,0" },
+  I: {
+    shape: [
+      [0, "I", 0, 0],
+      [0, "I", 0, 0],
+      [0, "I", 0, 0],
+      [0, "I", 0, 0],
+    ],
+    color: "80,227,230",
+  },
+  J: {
+    shape: [
+      [0, "J", 0],
+      [0, "J", 0],
+      ["J", "J", 0],
+    ],
+    color: "132,61,198",
+  },
+  Z: {
+    shape: [
+      ["Z", "Z", 0],
+      [0, "Z", "Z"],
+      [0, 0, 0],
+    ],
+    color: "227,78,78",
+  },
+};

dbd8afe2fa9dfa91071204025c832b92568c28ee
zerochae <zerochae@kakao.com>
2022-03-23 19:23:37 +0900

    Feat: root index set

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import GlobalStyle from "Styles/Global";
+
+import App from "src/Pages/App/index";
+
 ReactDOM.render(
-  <React.StrictMode></React.StrictMode>,
+  <React.StrictMode>
+    <GlobalStyle />
+    <App />
+  </React.StrictMode>,
   document.getElementById("root")
 );
